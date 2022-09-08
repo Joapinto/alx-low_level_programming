074432aad56480effd180ff7a1bf7a6d90aff3bb
Joseph Udin <josephudin2@gmail.com>
2022-09-08 14:48:42 -0700

    Low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                 
-/**                                                                
- * main - Entry point                                        
- * Description: "This program outputs a strings"    
- * Return: Always 0 (Success)                                  
- * */                                                          
-int main(void)                                                     
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "This program outputs a strings"
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
