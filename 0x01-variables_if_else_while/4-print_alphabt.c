5f6a25f75c7c64f9b95f87b5572415e85213502b
Joseph Udin <josephudin2@gmail.com>
2022-09-09 18:53:44 -0700

    If else variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -entry block
+ * main - entry point
  *
  * Return: alway returns 0
  *
@@ -10,10 +10,13 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
 		putchar(a);
-	for (a = 'A'; a <= 'Z'; a++)		                   
-	        putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
-	
