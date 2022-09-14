135b6bc19a5c914548a54f4400b7083f34026446
Joseph Udin <josephudin2@gmail.com>
2022-09-14 16:47:12 -0700

    Nested Loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,15 +12,14 @@ int main(void)
 
 	j = 1;
 	k = 2;
-	sum = 0;
-
+	sum = 0;	
 	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
 			sum = sum + j;
 		}
-		sum = sum + j;
+		next = j + k;
 		j = k;
 		k = next;
 	}
@@ -29,3 +28,4 @@ int main(void)
 
 	return (0);
 }
+
